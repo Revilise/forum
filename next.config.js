a8458640fc0204e4925b708018f12cb1b0e00f40
Revilise <nice.mutnykh@mail.ru>
2022-11-12 13:58:44 +0300

    db + auth session

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        DB_USER: 'postgres',
+        DB_PASS: 'root',
+        DB_HOST: 'localhost',
+        DB_PORT: '5432',
+        DB_NAME: 'forum_db',
+
+        SESSION_PASSWORD: 'strong-pass-ever',
+        SESSION_COOKIE: 'forum/auth',
+
+        NODE_ENV: 'dev' || 'production',
+
+        NEXT_PUBLIC_APP_HOSTNAME: 'localhost:3000'
+    }
+}
