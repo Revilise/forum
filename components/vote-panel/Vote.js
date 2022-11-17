94124825d3ba48ee294cfd1a54b426e1e6366ceb
Revilise <nice.mutnykh@mail.ru>
2022-11-18 00:21:08 +0300

    with useState

--- components/vote-panel/Vote.js
+++ components/vote-panel/Vote.js
@@ -2,31 +2,35 @@ import cl from './vote-panel.module.scss';
 import Icons from "../icon/icons";
 import {useEffect} from "react";
 
-function VoteHOC(WrappedComponent, selectData) {
-    return function() {
-        const { vote, setVote } = selectData();
+function VoteHOC(WrappedComponent, setVote, vote) {
 
+    // const { vote, setVote } = useVote();
+
+    useEffect(() => {
+
+    }, [vote]);
+
+    return function() {
         const onVote = (value) => {
             // react query to api for updating value
             // then
             // redux changes store
-            setVote(vote + value);
+            setVote(value);
         }
+
         const onUnvote = () => {
             // react query to api for updating value into 0
             // then
             // redux changes store
             setVote(0);
         }
-        console.log("data updated: "+ vote)
 
         return <WrappedComponent value={vote} onVote={onVote} onUnvote={onUnvote} />
     }
 }
 
-export const VoteUp = ({selectData}) => VoteHOC(
+export const VoteUp = ({setVote, vote}) => VoteHOC(
     function({value, onVote, onUnvote}) {
-
         const clause = value === 1;
 
         return (
@@ -38,10 +42,10 @@ export const VoteUp = ({selectData}) => VoteHOC(
             </button>
         )
     },
-    selectData
+    setVote, vote
 )()
 
-export const VoteDown = ({selectData}) => VoteHOC(
+export const VoteDown = ({setVote, vote}) => VoteHOC(
     function({value, onVote, onUnvote}) {
         const clause = value === -1;
 
@@ -54,5 +58,5 @@ export const VoteDown = ({selectData}) => VoteHOC(
             </button>
         )
     },
-    selectData
+    setVote, vote
 )()
--- components/vote-panel/VotePanel.js
+++ components/vote-panel/VotePanel.js
@@ -2,18 +2,19 @@ import cl from './vote-panel.module.scss';
 import {VoteDown, VoteUp} from "./Vote";
 import {useEffect, useState} from "react";
 
-export default function VotePanel({vote}) {
 
-    function useVote() {
-        const [value, setVote] = useState(vote);
 
-        return { vote: value, setVote }
-    }
+export default function VotePanel(props) {
+
+    const [ vote, setVote ] = useState(props.vote);
 
     return (
         <div className={cl.container}>
-            <VoteUp selectData={useVote}/>
-            <VoteDown selectData={useVote} />
+            {/*<VoteUp vote={vote} useVote={useVote} />*/}
+            {/*<VoteDown vote={vote} useVote={useVote} />*/}
+
+            <VoteUp vote={vote} setVote={setVote}/>
+            <VoteDown vote={vote} setVote={setVote} />
         </div>
     )
 }
