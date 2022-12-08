ad8006ff2700326b6837daad69954d9fd37b0ae7
Revilise <nice.mutnykh@mail.ru>
2022-12-08 21:52:57 +0300

    popup react on escape

--- features/popup/Popup.js
+++ features/popup/Popup.js
@@ -2,10 +2,21 @@ import cl from './popup.module.scss';
 import Icons from "../../components/icon/icons";
 import {useDispatch, useSelector} from "react-redux";
 import {closePopup, selectContent} from "./PopupSlice";
+import {useEffect} from "react";
 
 export default function Popup() {
     const dispatch = useDispatch();
     const {text, header} = useSelector(selectContent);
+
+    useEffect(() => {
+        function KeyDown(e) {
+            if (e.key === "Escape") close();
+        }
+
+        window.addEventListener('keydown', KeyDown);
+
+        return () => window.removeEventListener('keydown', KeyDown);
+    }, [])
 
     function close() {
         dispatch(closePopup())
